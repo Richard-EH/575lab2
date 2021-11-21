1fcb3d5cde1ecd349f2f1cf3e3e37c731754d4db
Richard-EH <83168110+Richard-EH@users.noreply.github.com>
2021-11-21 17:41:06 -0500

    Initial File Commit

---
+++ d3-lab/js/main.js
@@ -0,0 +1,47 @@
+let files = ["data/Local_Covid.csv", "data/VA_County_Amend.topojson"]
+let promises = [];
+let countyData = null;
+let attributeData = null;
+let expressed = "Population"
+let attributeDataArray = [];
+
+// begin script attempt
+window.onload = function()
+{
+    files.forEach(function(url){
+        promises.push(d3.json(url))
+    });
+
+    Promise.all(promises).then(function(values){
+        values.forEach(element => {
+            if (element.arcs != undefined)
+            {
+                element = topojson.feature(element, element.objects["NAME"])
+                countyData = element;
+            }
+            else {
+                attributeData = element;
+            }
+            console.log(element)
+        });
+
+        joinData(attributeData, countyData.features);
+        loadMap (countyData, attributeData, expressed);
+    })
+};
+
+// Loads when the window is done. 
+// window.onload = setMap();
+
+// function setMap(){
+
+//     d3.queue()
+//         .defer(d3.csv, "data/Local_Covid.csv")
+//         .defer(d3.json, "data/VA_County_Amend.json")
+//         .await(callback);
+//     function callback( error, csvData, countyData){
+//         console.log(error);
+//         console.log(csvData);
+//         console.log(countyData);
+//     };
+
