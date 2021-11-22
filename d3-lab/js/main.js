4c23d670ded330fc43d9e21254999887bd6a7e60
Richard-EH <83168110+Richard-EH@users.noreply.github.com>
2021-11-21 19:56:56 -0500

    More Code Correction Attempts

--- d3-lab/js/main.js
+++ d3-lab/js/main.js
@@ -3,32 +3,29 @@ window.onload = setMap();
 
 function setMap(){
 // Dimensions for those who don't know
-    var width = 960;
+    var width = window.innerWidth * 0.5;
     var height = 460;
 // creation of the SVG area
     var map = d3.select("body")
         .append("svg")
+        .attr("class", "map")
         .attr("width", width)
         .attr("height", height);//this code at least works.
-//queue block however I have been unable to get this to load data properly. 
-    d3.queue()
-        .defer(d3.csv, "/d3-lab/data/Local_Covid.csv")
-        .defer(d3.json, "/d3-lab/data/VA_County_Amend.topojson")
-        .await(callback);
-    // Callback Function to run after the d3.queue
-    function callback( error, csvData, countyData){
-        // Only error has any data...everything else is returned as undefined and I have no idea how to fix this. Error only returns the first row of the csv. 
-        console.log(error);
-        console.log(csvData);
-        console.log(countyData);
 
-    var counties = map.selectAll(".counties")
-        .data (countyData)
-        .enter()
-        .append("path")
-        .attr("class", function(d){
-            return "counties" + d.properties.FIPS;
-        })
-        .attr("d",path);
-    };
+	//use Promise.all to parallelize asynchronous data loading
+	var promises = [];
+	promises.push(d3.csv("/d3-lab/data/Local_Covid.csv")); //load attributes from csv
+	promises.push(d3.json("/d3-lab/data/VA_County_Amend.topojson")); //load choropleth spatial data
+	Promise.all(promises).then(callback);
+    // Callback Function to run after the promise code block
+    function callback(data){
+        // Only error has any data...everything else is returned as undefined and I have no idea how to fix this. Error only returns the first row of the csv. 
+        [csvData,countyData] = data;
+//graticule
+        setGraticule(map, path);
+        // topo conversion
+        var vaCounty = (topojson.feature(county, county.objects.VA_County_Amend).features)};
+
+        vaCounty = joinData(vaCounty, csvData);
+
 };
